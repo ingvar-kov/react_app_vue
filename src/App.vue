5bad168315f54454b7ade809d74b1c1d7e2cfdcf
Ingvar <my@example.com>
2025-04-14 15:04:58 +0300

    develop user component added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script>
+  import User from './components/User.vue'
+
   export default {
+    components: { User },
     data() {
       return {
         users: [],
@@ -27,46 +30,61 @@
           pass: this.user_pass,
           mail: this.user_mail
         })
+      },
+      delete_user(index) {
+        this.users.splice(index, 1);
       }
     }
   }
 </script>
 
 <template>
-  <input type="text" v-model="user_name" placeholder="Имя">
-  <input type="password" v-model="user_pass" placeholder="Пароль">
-  <input type="email" v-model="user_mail" placeholder="E-mail">
-  <p className="error">{{ error }}</p>
-  <button @click="send_data()">Отправить</button>
-  <div v-if="users.length == 0" className="user">
-    <p>Нет активных пользователей</p>
-  </div>
-  <div v-for="(el, index) in users" :key="index" className="user">
-    <h3>{{ el.name }}</h3>
-    <p>New user added</p>
+  <div className="wrapper">
+    <input type="text" v-model="user_name" placeholder="Имя">
+    <input type="password" v-model="user_pass" placeholder="Пароль">
+    <input type="email" v-model="user_mail" placeholder="E-mail">
+    <p className="error">{{ error }}</p>
+    <button @click="send_data()">Отправить</button>
+    <div v-if="users.length == 0" className="user">
+      <p>Нет активных пользователей</p>
+    </div>
+    <User v-for="(el, index) in users" :key="index" :user="el" :index="index" :delete_user="delete_user"/>
   </div>
 </template>
 
 <style scoped>
+  div {
+    margin: 0 auto;
+    padding: 0 auto;
+  }
   input {
     margin: 20px;
     padding: 10px;
+    border-style: none;
     border-radius: 5px;
+    border-color: rgb(103, 85, 119);
   }
   button {
     display: block;
     margin: 20px;
     padding: 10px;
     color: rgb(43, 43, 43);
-    background-color: rgb(68, 236, 68);
+    background-color: rgb(165, 255, 165);
     font-weight: 600;
     border-radius: 5px;
     border: none;
   }
-  .user {
+  .wrapper {
+    padding: 40px;
     margin: 10px;
+    margin-top: 20px;
+    background-color: rgb(198, 169, 236);
+    border-radius: 20px;
+  }
+  .user {
+    margin: 20px;
     padding: 20px;
-    background-color: rgb(191, 191, 255);
+    background-color: rgb(227, 205, 255);
     border-radius: 5px;
     border: none;
     display: inline-block;
---
+++ src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div className="user">
+        <button @click="delete_user(index)">Удалить</button>
+        <h3>{{ user.name }}</h3>
+        <p>{{ user.mail }} - <b>{{ user.pass }}</b></p>
+        <p>New user added</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            delete_user: {
+                type: Function,
+                required: true
+            },
+            user: {
+                type: Object,
+                required: true
+            },
+            index: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    button {
+        border: none;
+        margin-top: 10px;
+        margin-right: 10px;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: rgb(255, 162, 162);
+        color: rgb(122, 0, 0);
+        font-weight: bold;
+        cursor: pointer;
+        float: right;
+        transition: 500ms ease;
+    }
+    button:hover {
+        transform: translateY(-3px);
+        background-color: rgb(255, 218, 218);
+    }
+</style>
